7e267dccef53f280022a78c01c626d163c2516be
William <snoman3010@gmail.com>
2021-02-17 14:56:03 -0500

    Wrote out framework for javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+let cellContents = [false,false,false,false,false,false,false,false,false]
+let playerX = 'X';
+let playerO = 'O';
+let vsComputer = false;
+let currentPlayer = 'X';
+function startGame(){
+
+}
+function chooseCell(){
+
+}
+function checkWin(){
+
+}
+function incrementTimer(){
+
+}
+function cpuTurn(){
+
+}
